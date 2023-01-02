012445d9ea86da80d5274ca4358b9e010ad837d8
Mangar <Mangar@RYZEN9>
2023-01-02 21:24:08 +0100

    styling

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 ng build --base-href /yahagui/
-del -r z:\yahaGUI2\*.*
-copy -r c:\Development\yahaGUI2\yahaGUI2\dist\yaha-gui2\*.* z:\yahaGUI2\
+robocopy c:\Development\yahaGUI2\yahaGUI2\dist\yaha-gui2 z:\yahaGUI2 *.* /MIR
